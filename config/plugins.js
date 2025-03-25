30846d7e4695bd84876a2df52ba6f59e3aeaf8e6
Mehdi <mehdi@MacBook-Pro-de-Mehdi.local>
2025-03-25 14:47:29 +0100

    cloudinary timeout

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,23 @@
-module.exports = () => ({});
+const { env } = require("@strapi/utils");
+
+module.exports = () => ({
+  // ... Éventuelles autres clefs
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME"),
+        api_key: env("CLOUDINARY_KEY"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {
+          timeout: 1000 * 60 * 3, // = 3 minutes
+        },
+        delete: {},
+      },
+    },
+  },
+  // ... Éventuelles autres clefs
+});
